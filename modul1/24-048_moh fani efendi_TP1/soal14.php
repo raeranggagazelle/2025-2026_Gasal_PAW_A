9de1738db5e21ff2982ae02d338fcfd0a0c0d7c9
faniefendi <efendifani3@gmail.com>
2025-09-30 21:04:00 +0700

    Add files via upload

---
+++ modul1/24-048_moh fani efendi_TP1/soal14.php
@@ -0,0 +1,12 @@
+<?php
+function familyName($fname) {
+  echo "$fname Refsnes.<br>";
+}
+
+// pemanggilan fungsi dengan argumen berbeda
+familyName("Jani");
+familyName("Hege");
+familyName("Stale");
+familyName("Kai Jim");
+familyName("borge");
+?>
