ab823f466cd00a203eff58e54c7c1b2d361b3f59
24-134 Salwa Sekar Gading <salwasekar00@gmail.com>
2025-09-30 22:17:04 +0700

    Add files via upload

--- modul-1/24-134_Salwa_Sekar_Gading/4.php
+++ modul-1/24-134_Salwa_Sekar_Gading/4.php
@@ -5,8 +5,8 @@
 <?php
 $color = "red";
 echo "My car is " . $color . "<br>";
-echo "My house is " . $color . "<br>";
-echo "My boat is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
 ?>
 
 </body>
